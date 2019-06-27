be890959e6011a9deb73f21ca1be9b2a45ff2d27
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-27 14:55:49 -0500

    add some styling

--- src/components/StellarAccount.js
+++ src/components/StellarAccount.js
@@ -1,22 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 function StellarAccount(props) {
-
-  useEffect(() => {
-
-  }, [props.stellar.key])
+  useEffect(() => {}, [props.stellar.key]);
 
   return (
-    <div>Key: {props.stellar.key}</div>
-  )
+    <section className="stellarAccount">
+      <h1>Key:</h1>
+      <p style={{ paddingTop: "20px" }}>{props.stellar.key}</p>
+    </section>
+  );
 }
 
 const mapStateToProps = (state, ownProps) => ({
   stellar: state.stellar
 });
 
-export default connect(
-  mapStateToProps,
-)(StellarAccount)
+export default connect(mapStateToProps)(StellarAccount);
