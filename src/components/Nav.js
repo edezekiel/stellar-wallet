889620158dbb436fa1dc5e57432710395d650980
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-27 15:52:35 -0500

    rename nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,10 +9,10 @@ function Nav(props) {
         <h1>Home</h1>
       </Link>
       <Link to="/account">
-        <h1>Account</h1>
+        <h1>Balance</h1>
       </Link>
       <Link to="/create">
-        <h1>Create</h1>
+        <h1>New Account</h1>
       </Link>
     </nav>
   );
