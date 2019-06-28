3f4cb27b0ae42dd64700a3480d3d11bc3db31173
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-28 11:39:16 -0500

    refactor the home page

---
+++ src/stellarSDK/createEscrowAccount.js
@@ -0,0 +1,8 @@
+import StellarSdk from "stellar-sdk";
+
+import { createPair } from "../stellarSDK/createPair";
+import { createAccount } from "../stellarSDK/createAccount";
+
+const createEscrowAccount = async (source) {
+
+}
--- src/stellarSDK/createPayment.js
+++ src/stellarSDK/createPayment.js
@@ -1,6 +1,6 @@
 import StellarSdk from "stellar-sdk";
 
-export default async function createTransaction(tx) {
+export default async function createPayment(tx) {
   StellarSdk.Network.useTestNetwork();
   const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
   const sourceKeys = StellarSdk.Keypair.fromSecret(tx.key);
