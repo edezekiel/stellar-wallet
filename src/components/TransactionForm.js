e1a82bbf2e0d87d592a889cbeba5505826e34470
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-27 19:01:57 -0500

    successfully send payment using the wallet

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createTx } from "../redux/actions";
-import { createTransaction } from "../stellarSDK/createTransaction";
+import createTransaction from "../stellarSDK/createTransaction";
 
 import Layout from "./Layout";
 
--- src/stellarSDK/createTransaction.js
+++ src/stellarSDK/createTransaction.js
@@ -1,8 +1,31 @@
 import StellarSdk from "stellar-sdk";
 
-export const createTransaction = tx => {
+export default async function createTransaction(tx) {
   StellarSdk.Network.useTestNetwork();
   const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
-  const sourceKeys = StellarSdk.Keypair.fromSecret(tx.key)
-  console.log(sourceKeys)
-};
+  const sourceKeys = StellarSdk.Keypair.fromSecret(tx.key);
+  const baseFee = await server.fetchBaseFee();
+
+  const account = await server.loadAccount(sourceKeys.publicKey());
+
+  const transaction = new StellarSdk.TransactionBuilder(account, { fee: baseFee })
+    .addOperation(
+      StellarSdk.Operation.payment({
+        destination: tx.destination,
+        asset: StellarSdk.Asset.native(),
+        amount: tx.amount
+      })
+    )
+    .addMemo(StellarSdk.Memo.text(tx.memo))
+    .setTimeout(180)
+    .build();
+
+  transaction.sign(sourceKeys);
+
+  try {
+    const transactionResult = await server.submitTransaction(transaction);
+    console.log("Success! Results:");
+  } catch (error) {
+    console.error("Something went wrong!", error);
+  }
+}
